3e3c248133b3579461c27ac514f3e57503c0c3b2
pavithra <pavithradeepika301@gmail.com>
2023-02-20 14:41:17 +0530

    create button from  mui

--- src/Test.js
+++ src/Test.js
@@ -1,16 +1,32 @@
-import React from 'react'
-import Typography from '@mui/material/Typography';
+import React from "react";
+import Typography from "@mui/material/Typography";
+import Button from '@mui/material/Button';
 
 const Test = () => {
   return (
     <div>
-
-    <Typography>
-        hello world
-      </Typography>
+      <div>
+        <Typography
+          variant="h4"
+          sx={{ color: "red", border: "1px solid black", borderRadius: 5 }}
+        >
+          hello world
+        </Typography>
+      </div>
+      <br></br>
+      <div>
+      <Button onClick={() =>alert("Button clicked") } variant="text" color="secondary" size="large" sx={{ margin:3 }}>
+        First
+        </Button>
+      <Button onClick={() =>alert("Button clicked") } variant="contained"  color="success" size="medium" sx={{ margin:3 }}>
+        Second
+      </Button>
+      <Button onClick={() =>alert("Button clicked") } variant="outlined" color="error" size="small" sx={{ margin:3 }}>
+        Third
+      </Button>
+      </div>
     </div>
+  );
+};
 
-  )
-}
-
-export default Test
+export default Test;
