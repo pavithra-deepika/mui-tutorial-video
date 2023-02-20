e084eb0a9e624a8738575200ee2c2c9c5e66411b
pavithra <pavithradeepika301@gmail.com>
2023-02-20 19:38:47 +0530

    create form handling from mui

--- src/Form.js
+++ src/Form.js
@@ -1,37 +1,58 @@
-import React, { useState } from 'react'
-import TextField from '@mui/material/TextField';
-import { Typography } from '@mui/material';
-
+import React, { useState } from "react";
+import TextField from "@mui/material/TextField";
+import { Button, Typography } from "@mui/material";
 
 const Form = () => {
-    const [name, setName] = useState("")
-   
+  const [input, setInput] = useState({
+    firstName: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    setInput({ ...input, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(input);
+  };
+
   return (
     <div>
-        <TextField 
-    
+      <form onSubmit={handleSubmit}>
+        <TextField
+          id="firstName"
+          name="firstName"
           variant="outlined"
-          placeholder='Name' 
-          sx={{ margin:3 }} 
-          value={name} onChange={(e) => setName(e.target.value)} 
-          />
-        <TextField 
-         variant="standard" 
-         placeholder='password' 
-         sx={{ margin:3 }}
-          />
-        <TextField  
-        variant="filled" 
-        placeholder='Email' 
-        sx={{ margin:3 }} 
-           />
-        <Typography>
-            {name}
-        </Typography>
-
-
+          placeholder="Name"
+          sx={{ margin: 3 }}
+          value={input.firstName}
+          onChange={handleChange}
+        />
+        <TextField
+          id="password"
+          name="password"
+          variant="standard"
+          placeholder="password"
+          sx={{ margin: 3 }}
+          value={input.password}
+          onChange={handleChange}
+        />
+        <TextField
+          id="email"
+          name="email"
+          variant="filled"
+          placeholder="Email"
+          sx={{ margin: 3 }}
+          value={input.email}
+          onChange={handleChange}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Form
+export default Form;
