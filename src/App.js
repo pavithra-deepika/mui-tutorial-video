0121807400c22ef36f3e2306ee52befcb4c232b4
pavithra <pavithradeepika301@gmail.com>
2023-02-24 13:40:52 +0530

    complete Autocomplete in mui

--- src/App.js
+++ src/App.js
@@ -8,38 +8,44 @@ import Dialogs from "./Dialog";
 import Modal from "./Modal";
 import Lists from "./List";
 import NestedLists from "./NestedLists";
+import Ac from "./Ac";
 function App() {
   return (
-    <div className="App">
-      <h1> MUI </h1>
-      <br></br>
-      {/* <div>
-        <h3>Tabs</h3>
-        <TabsMui />
-      </div> */}
-      <h3>App Bar</h3>
-      <App_Toolbar />
-      <div>
-        <h3>Typography & Button </h3>
-        <br></br>
-        <Test />
-        <h3>Form</h3>
-        <Form />
-        <h3>Menu</h3>
-        <Menu />
-      </div>
-      <div>
-        <h1>card</h1>
-        <Card />
-      </div>
-      <br></br>
-      <Dialogs />
-      <br></br>
-      <Modal />
-      <br></br>
-      <Lists />
-      {/* <NestedLists /> */}
+    <div>
+      <Ac />
     </div>
+      // <AutoCom />
+    
+    // <div className="App">
+    //   <h1> MUI </h1>
+    //   <br></br>
+    // <div>
+    //     <h3>Tabs</h3>
+    //     <TabsMui />
+    //   </div>
+    //   <h3>App Bar</h3>
+    //   <App_Toolbar />
+    //   <div>
+    //     <h3>Typography & Button </h3>
+    //     <br></br>
+    //     <Test />
+    //     <h3>Form</h3>
+    //     <Form />
+    //     <h3>Menu</h3>
+    //     <Menu />
+    //   </div>
+    //   <div>
+    //     <h1>card</h1>
+    //     <Card />
+    //   </div>
+    //   <br></br>
+    //   <Dialogs />
+    //   <br></br>
+    //   <Modal />
+    //   <br></br>
+    //   <Lists />
+    //   <NestedLists /> 
+    // </div>
   );
 }
 
